b4ace50c4cd9264effa8712d1e939cd6bf9210d3
Sena Lim <tpsk8322@naver.com>
2023-04-11 15:31:09 +0900

    16-14 수정

--- src/chap16/16-14.js
+++ src/chap16/16-14.js
@@ -1,7 +1,7 @@
 function deepFreeze(target) {
     if (target && typeof target === 'object' && !Object.isFrozen(target)) {
         Object.freeze(target);
-        Object.keys(target).forEach(deepFreeze(target[key]));
+        Object.keys(target).forEach(key => deepFreeze(target[key]));
     }
     return target;
 }
