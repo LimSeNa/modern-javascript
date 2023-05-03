556244fd84ea43e51641d3017d0374d8eda03618
Sena Lim <tpsk8322@naver.com>
2023-05-04 01:01:56 +0900

    프로퍼티 존재 확인 - hasOwnProeprty 메서드

---
+++ src/chap19/19-62.js
@@ -0,0 +1,5 @@
+const person = { name: 'Lee' };
+
+console.log(person.hasOwnProperty('name')); // true
+console.log(person.hasOwnProperty('age')); // false
+console.log(person.hasOwnProperty('toString')); // false
